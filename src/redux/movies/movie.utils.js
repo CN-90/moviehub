c6e8b28f50f320d545d72c77a84052710f85da1e
Christian <chr.revel@gmail.com>
2020-01-02 09:23:50 -0700

    Removed config

--- src/redux/movies/movie.utils.js
+++ src/redux/movies/movie.utils.js
@@ -1,15 +1,14 @@
 import Axios from 'axios';
-import apiKey from './../../config/key';
 
 //Fetch movie details and crew details about specific movie.
 export const fetchMovieAsync = async id => {
   let movieInfo, movieCredits;
   try {
     movieInfo = await Axios.get(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US&append_to_response=videos`
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${''}&language=en-US&append_to_response=videos`
     );
     movieCredits = await Axios.get(
-      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`
+      `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${''}`
     );
   } catch (err) {
     console.log(err);
@@ -21,12 +20,12 @@ export const fetchMovieAsync = async id => {
 //Fetch all movies under a specific category - e.g Upcoming movies, popular movies, etc...
 export const fetchMoviesAsync = (category, pageNumber) => {
   return Axios.get(
-    `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&page=${pageNumber}`
+    `https://api.themoviedb.org/3/movie/${category}?api_key=${''}&language=en-US&page=${pageNumber}`
   );
 };
 
 export const fetchMoviesSearchAsync = (searchTerm, pageNumber) => {
   return Axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchTerm}&page=${pageNumber}&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${''}&language=en-US&query=${searchTerm}&page=${pageNumber}&include_adult=false`
   );
 };
